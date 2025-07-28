584603a95e152161fb433b11c9b1500b9146bf3d
suebin78 <ksb060708@hanyang.ac.kr>
2025-07-28 14:44:22 +0900

    last comment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo.trim() !== "") {
+      setToDos((currentArray) => [
+        ...currentArray,
+        { text: toDo, checked: false },
+      ]);
+      setToDo("");
+    }
+  };
+  const onToggleCheck = (index) => {
+    setToDos((currentArray) => {
+      return currentArray.map((item, i) => {
+        if (i === index) {
+          return { ...item, checked: !item.checked };
+        }
+        return item;
+      });
+    });
+  };
+  const onRemove = (index) => {
+    setToDos((currentArray) => currentArray.filter((_, i) => i !== index));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <h1 className="title">Todo List</h1>
+        <form className="form-box" onSubmit={onSubmit}>
+          <input
+              id="todo"
+              className="todo-input"
+              type="text"
+              value={toDo}
+              onChange={onChange}
+          />
+          <button id="addBtn" className="add-btn" type="submit"></button>
+        </form>
+        <div id="todoBox" className="todo-box">
+          {toDos.map((item, index) => (
+              <label
+                  className={`new-todo ${item.checked ? "check" : ""}`}
+                  key={index}
+              >
+                {item.text}
+                <input
+                    type="checkbox"
+                    checked={item.checked}
+                    onChange={() => onToggleCheck(index)}
+                />
+                <span className="check-mark"></span>
+                <button className="rm-btn" onClick={() => onRemove(index)}></button>
+              </label>
+          ))}
+        </div>
+      </>
   );
 }
 
 export default App;
+
